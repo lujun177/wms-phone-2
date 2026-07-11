f672b26ed37ef136e32f43f87cc1a9b2db2bc7ca
alone <lujun128340520@qq.com>
2026-07-12 01:24:05 +0700

    Add files via upload

---
+++ app/API/GOODS/route.js
@@ -0,0 +1,14 @@
+export async function GET(request) {
+  const { searchParams } = new URL(request.url);
+  const sku = searchParams.get('sku');
+  
+  const url = `https://khovpgqqrltmiclwzec.supabase.co/rest/v1/goods?sku=eq.${sku}&select=*`;
+  const res = await fetch(url, {
+    headers: {
+      'apikey': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imtob3ZwZ3FxcmlsdG1pY2x3emVjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODM3NzMyNDQsImV4cCI6MjA5OTM0OTI0NH0.H1BAyz93efH4FRC6TzBgR9RF8Qnhmps8WCdltvc-W9k',
+      'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imtob3ZwZ3FxcmlsdG1pY2x3emVjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODM3NzMyNDQsImV4cCI6MjA5OTM0OTI0NH0.H1BAyz93efH4FRC6TzBgR9RF8Qnhmps8WCdltvc-W9k'
+    }
+  });
+  const data = await res.json();
+  return Response.json(data);
+}
